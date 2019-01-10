4c45b9b5609473c4d00f7f70cd501bb87df4f7fb
Samuel Bering <samuelbering@hotmail.com>
2019-01-10 16:42:07 +0100

    Added dynamic edit of slider content area and Background image etc.

---
+++ WebDesigner/Models/Media/ImageFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+
+namespace WebDesigner.Models.Media
+{
+    [ContentType(DisplayName = "Image File", GUID = "0f9ebd84-65c9-4c7b-aa8a-c1989cd67a16", 
+        Description = "Use this to upload image files.")]
+    [MediaDescriptor(ExtensionString = "png,gif,jpg,jpeg")]
+    public class ImageFile : ImageData
+    {
+
+        [CultureSpecific]
+        [Editable(true)]
+        public virtual string Description { get; set; }
+
+    }
+}
