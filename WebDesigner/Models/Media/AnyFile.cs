56a419144e034d61b993bc3d046e55f4f08d8583
Samuel Bering <samuelbering@hotmail.com>
2019-01-10 15:35:59 +0100

    Localized startpage Added any file type upload support

---
+++ WebDesigner/Models/Media/AnyFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace WebDesigner.Models.Media
+{
+    [ContentType(DisplayName = "Any File", GUID = "78577daa-69b4-4493-a551-3a9a0f479565",
+        Description = "Use this to upload any type of file.")]
+    
+    public class AnyFile : MediaData
+    {
+       
+    }
+}
--- WebDesigner/Models/Pages/StartPage.cs
+++ WebDesigner/Models/Pages/StartPage.cs
@@ -13,10 +13,10 @@ namespace WebDesigner.Models.Pages
     [AvailableContentTypes(Include = new[] { typeof(StandardPage) })]
     public class StartPage : SitePageData
     {
-        [CultureSpecific]
-        [Display(Name = "Heading", Description = "If the Heading is not set, the page falls back to showing the Name.",
-            GroupName = SystemTabNames.Content, Order = 10)]
-        public virtual string Heading { get; set; }
+        //[CultureSpecific]
+        //[Display(Name = "Heading", Description = "If the Heading is not set, the page falls back to showing the Name.",
+        //    GroupName = SystemTabNames.Content, Order = 10)]
+        //public virtual string Heading { get; set; }
      
         [CultureSpecific]
         [Display(Name = "Footer text",
