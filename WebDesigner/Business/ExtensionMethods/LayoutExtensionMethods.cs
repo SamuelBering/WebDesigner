15b339790d0980630610dc888e52620b4feafd3b
Samuel Bering <samuelbering@hotmail.com>
2019-01-16 20:11:15 +0100

    Fixed footer links to all pages

--- WebDesigner/Business/ExtensionMethods/LayoutExtensionMethods.cs
+++ WebDesigner/Business/ExtensionMethods/LayoutExtensionMethods.cs
@@ -4,25 +4,64 @@ using EPiServer.Core;
 using EPiServer.ServiceLocation;
 using System.Web.Mvc;
 using System.Text;
+using EPiServer.Filters;
+using System.Linq;
+using EPiServer.Web.Routing;
 
 namespace WebDesigner.Business.ExtensionMethods
 {
     public static class LayoutExtensionMethods
     {
-        private static void RenderFooterText( StringBuilder stringBuilder)
+        private static void RenderFooterText(StringBuilder stringBuilder, SitePageData page, IContentLoader loader)
         {
+            stringBuilder.Append($"{CreateLink(page)}");
+            var childPages = FilterForVisitor
+               .Filter(loader.GetChildren<SitePageData>(page.ContentLink))
+               .Cast<SitePageData>().Where(p => p.VisibleInMenu);
 
+            if (childPages != null && childPages.Count() > 0)
+            {
+                stringBuilder.Append("<ul>");
+                foreach(var childPage in childPages)
+                {
+                    stringBuilder.Append("<li>");
+                    RenderFooterText(stringBuilder, childPage, loader);
+                    stringBuilder.Append("</li>");
+                }
+                stringBuilder.Append("</ul>");
+            }
         }
 
-        public static string RenderFooterText(this HtmlHelper helper, IContentLoader loader)
+        private static string CreateLink(SitePageData page)
         {
-            //StringBuilder stringBuilder = new StringBuilder();
+            var urlResolver = ServiceLocator.Current.GetInstance<UrlResolver>();
+            return $"<a href='{urlResolver.GetUrl(page.ContentLink)}'>{page.Name}</a>";
+        }
+
+        public static MvcHtmlString RenderFooterText(this HtmlHelper helper, IContentLoader loader)
+        {
             
+
+            StringBuilder stringBuilder = new StringBuilder();
             var startPage = loader.Get<StartPage>(ContentReference.StartPage);
-            //stringBuilder.Append($"{startPage.Name}");
-            
-            //RenderFooterText(stringBuilder)
-            return startPage.FooterText;
+            stringBuilder.Append($"<div class='row'><div class='col-md-12'>{startPage.FooterText}</div></div><hr>");
+
+            stringBuilder.Append($"<div class='row'><div class='col-md-2'>{CreateLink(startPage)}</div>");
+
+            var childPages = FilterForVisitor
+                .Filter(loader.GetChildren<SitePageData>(ContentReference.StartPage))
+                .Cast<SitePageData>().Where(page => page.VisibleInMenu);
+            if (childPages != null && childPages.Count() > 0)
+            {
+                foreach (var childPage in childPages)
+                {
+                    stringBuilder.Append("<div class='col-md-2'>");
+                    RenderFooterText(stringBuilder, childPage, loader);
+                    stringBuilder.Append("</div>");
+                }
+            }
+            stringBuilder.Append($"</div>");
+            return MvcHtmlString.Create(stringBuilder.ToString());
         }
     }
 }
